9f3d4bcbf6bab99a60e439301e205bb74f329490
kuoch934 <kuoch934@gmail.com>
2025-11-07 18:40:34 +0800

    original version

--- src/rosserial_python/nodes/serial_node.py
+++ src/rosserial_python/nodes/serial_node.py
@@ -47,8 +47,8 @@ if __name__=="__main__":
     rospy.init_node("serial_node")
     rospy.loginfo("ROS Serial Python Node")
 
-    port_name = rospy.get_param('~port','/dev/ttyUSB0')
-    baud = int(rospy.get_param('~baud','57600'))
+    port_name = rospy.get_param('~port','/dev/ttyACM0')
+    baud = int(rospy.get_param('~baud','460800'))
 
     # for systems where pyserial yields errors in the fcntl.ioctl(self.fd, TIOCMBIS, \
     # TIOCM_DTR_str) line, which causes an IOError, when using simulated port
